5f2fa425e38dbe191d29f5596fd89dd6f1b99dea
Dipesh_Maywade <dipesh@ideaclan.biz>
2021-09-14 10:02:40 +0530

    feat: dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,21 @@
+import { object } from 'prop-types'
 import React from 'react'
 import classes from './Burger.module.css'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 
-export default function Burger() {
+export default function Burger(props) {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(key => {
+            return [...Array(props.ingredients[key])].map((_, i) => {
+                return <BurgerIngredients key1 = {key +i} type= {key} />
+            });
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredients type= 'bread-top' />
-            <BurgerIngredients type= 'cheese' />
-            <BurgerIngredients type= 'meat' />
-            <BurgerIngredients type= 'bacon' />
+            {transformedIngredients}
             <BurgerIngredients type= 'bread-bottom' />
         </div>
     )
