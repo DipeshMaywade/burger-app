8ab6072d797859cea873436709fca1dc7a0c16b4
Dipesh_Maywade <dipesh@ideaclan.biz>
2021-09-14 11:36:30 +0530

    fix file import issue

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import classes from './Build.module.css'
+import classes from './BuildControls.module.css'
+import BuildControl from './BuildControl/BuildControl'
+
 
 export default function BuildControls(props) {
+
+    const controls = [
+        { label: 'Salad', type: 'salad' },
+        { label: 'Bacon', type: 'bacon' },
+        { label: 'Cheese', type: 'cheese' },
+        { label: 'Meat', type: 'meat' },
+    ];
+
     return (
-        <div>
-            
+        <div className = {classes.BuildControls}>
+            {controls.map(ctrl => (
+                <BuildControl key={ctrl.label} label={ctrl.label}/>
+            ))}
         </div>
     )
 }
